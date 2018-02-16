52aa7d04831b118838d0d3077ef33529a973dbca
Ismael <imartinr01@informatica.iesvalledeljerteplasencia.es>
2018-02-16 05:27:50 +0100

    Retoques

--- src/FramePrincipal.java
+++ src/FramePrincipal.java
@@ -1,15 +1,33 @@
-import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FramePrincipal {
 	private JFrame frame;
+	private BufferedImage canvas;
+	JButton btnTextoSimple;
+	JMenuItem itemTextoSimple;
 
 	public FramePrincipal() {
 		frame = new JFrame("");
@@ -24,19 +42,27 @@ public class FramePrincipal {
 	}
 
 	public void iniciarComponentes() {
-		frame.setLayout(new GridLayout());
+		frame.setLayout(new BorderLayout());
+		// -------------------------------------
 		// Barra d menu
 		JMenuBar menuBar = new JMenuBar();
 		// Anadir barra al frame
 		frame.setJMenuBar(menuBar);
+		// -------------------------------------
 		// MENU PRINCIPAL
 		JMenu menu1 = new JMenu("File");
 		menuBar.add(menu1);
+		// -------------------------------------
 		// ITEM MENU PRINCIPAL
-		JMenuItem itemTextoSimple = new JMenuItem("Texto 1");
+		itemTextoSimple = new JMenuItem("Texto 1");
+		itemTextoSimple.setEnabled(true);
+		itemTextoSimple.setMnemonic(KeyEvent.VK_C);
+		itemTextoSimple.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
 		menu1.add(itemTextoSimple);
+		// -------------------------------------
 		// SEPARAR
 		menu1.addSeparator();
+		// -------------------------------------
 		// CHECK BOX
 		JCheckBoxMenuItem check1 = new JCheckBoxMenuItem("Soy checkbox 1");
 		menu1.add(check1);
@@ -44,7 +70,7 @@ public class FramePrincipal {
 		menu1.add(check2);
 		// SEPARAR
 		menu1.addSeparator();
-		//RADIO BUTTON
+		// RADIO BUTTON
 		JRadioButtonMenuItem radio1 = new JRadioButtonMenuItem("Soy un botón 1");
 		menu1.add(radio1);
 		JRadioButtonMenuItem radio2 = new JRadioButtonMenuItem("Soy un botón 2");
@@ -59,9 +85,52 @@ public class FramePrincipal {
 		JMenuItem textoSubmenu = new JMenuItem("Texto submenu");
 		submenu.add(textoSubmenu);
 		menu1.add(submenu);
+		// -------------------------------------
+		// ToolBar Fuente
+		JToolBar toolbar = new JToolBar();
+		toolbar.setFloatable(false);
 
+		btnTextoSimple = new JButton("Texto");
+		btnTextoSimple.setEnabled(true);
+		try {
+			this.canvas = ImageIO.read(new File("img/ir.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		btnTextoSimple.setIcon(new ImageIcon(canvas.getScaledInstance(30, 30, Image.SCALE_SMOOTH)));
+		toolbar.add(btnTextoSimple);
+		
+		frame.add(toolbar, BorderLayout.NORTH);
+		
+		
+	
+
+		// -------------------------------------
+
+		frame.add(new TextArea());
 	}
 
 	public void iniciarListened() {
+		btnTextoSimple.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				itemTextoSimple.doClick();
+			}
+		});
+		
+		itemTextoSimple.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser();
+				chooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+				chooser.setFileFilter(new FileNameExtensionFilter("Ficheros de CSV", "csv"));
+				chooser.setAcceptAllFileFilterUsed(false);
+				if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+					System.out.println("Aprobado");
+					//manejador.setFicheroActual(chooser.getSelectedFile());
+				}
+			}
+		});
 	}
 }
